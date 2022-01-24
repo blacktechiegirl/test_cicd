d2085fcb6be7d8bb5af43980c6ef19e1c2619612
Aminat <amibarbs01@gmail.com>
2022-01-24 16:48:16 +0100

    second update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to Backend Development")
+    res.send("Welcome to CICD")
 });
 
 const port = process.env.port || 3000;
